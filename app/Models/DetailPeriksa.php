99688ef2694fe9b3c445b08a30287dfd42674ee0
Putra2203 <erdin22xii@gmail.com>
2025-12-26 01:23:46 +0700

    refactor(model): update obat and detail_periksa models

--- app/Models/DetailPeriksa.php
+++ app/Models/DetailPeriksa.php
@@ -8,10 +8,7 @@ class DetailPeriksa extends Model
 {
     protected $table = 'detail_periksa';
 
-    protected $fillable = [
-        'id_periksa',
-        'id_obat'
-    ];
+    protected $fillable = ['id_periksa', 'id_obat', 'jumlah'];
 
     public function periksa()
     {
