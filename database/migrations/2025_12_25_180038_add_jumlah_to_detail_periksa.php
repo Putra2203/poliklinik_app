a7abffe96458e664270cbe0941144f41a169009c
Putra2203 <erdin22xii@gmail.com>
2025-12-26 01:05:01 +0700

    feat(db): add stock column to obat and quantity column to detail_periksa

---
+++ database/migrations/2025_12_25_180038_add_jumlah_to_detail_periksa.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('detail_periksa', function (Blueprint $table) {
+            $table->integer('jumlah')->after('id_obat');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('detail_periksa', function (Blueprint $table) {
+        });
+    }
+};
